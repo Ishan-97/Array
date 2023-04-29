7a18d4f821430fc367b4ff852f805d0b6c33d919
Admin <Admin@DESKTOP-Q5UKC2J>
2023-04-29 10:05:44 +0300

    Добавьте файлы проекта.

---
+++ ConsoleAppli/ConsoleAppli.cpp
@@ -0,0 +1,29 @@
+﻿// ConsoleAppli.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#define N 10 
+int main()
+{
+    short arra[N][N]{};
+	for (size_t i = 0; i < N; ++i)
+	{
+		for (size_t j = 0; j < N; ++j)
+		{
+			if (i != j) {
+				arra [i][j] = 1;
+			}else{
+				arra[i][j] = 0;
+			}
+		}
+	}
+	for (size_t i = 0; i < N; i++)
+	{
+		for (size_t j = 0; j < N; j++)
+		{
+			std::cout << arra[i][j];
+		}
+		std::cout <<"\n";
+	}
+    return 0;
+}
